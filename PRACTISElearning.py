04c217cecd2cbae0320fcbc2af4fd38101197187
Ayan Das <ayan.das@voltbank.com.au>
2023-04-06 18:36:59 +1000

    NewMaterial

---
+++ PRACTISElearning.py
@@ -0,0 +1,24 @@
+message = 'The book of VANITAS'
+print(message)
+
+message2 = '''Unknown truth
+ of Vanitas'''
+print(message2)
+
+
+dog = 'Labrador'
+print(len(dog))
+
+
+message3 = "HUNTER X HUNTER"
+print(message3[0])
+print(message3[2])
+print(message3[6])
+
+greet = 'dogs and cats'
+print(greet.upper())
+
+msg = 'THE RATTIEST ULT OF VALORANT'
+print(msg.count('RATTIEST'))
+print(msg.count('T'))
+
